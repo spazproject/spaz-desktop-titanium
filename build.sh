bb3c39b9408a30462c227b3bd0df76eff5825026
Ed Finkler <coj@funkatron.com>
2011-01-02 20:37:26 -0500

    Add build & run bash script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SRCPATH='~/Applications/Projects/Spaz-Titanium'
+TMPPATH='/tmp/spaz'
+BLDPATH='/Users/coj/Sites/spaz-builds'
+TIPATH='/Users/coj/Library/Application Support/Titanium'
+SDKPATH='/Users/coj/Library/Application Support/Titanium/sdk/osx/1.1.0'
+BLDDATE=`date +%Y%m%d-%H%M%S%Z`
+
+# let user know where we'll be working
+echo "Source Path: ${SRCPATH}"
+echo "Temp Path: ${TMPPATH}"
+echo "Build Path:  ${BLDPATH}"
+echo "--------------------------------------"
+
+
+"${SDKPATH}/tibuild.py" -d "${BLDPATH}" -s "${TIPATH}" -r -a "${SDKPATH}" .
